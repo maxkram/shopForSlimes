e6a37df2583efdddee9b30efcf5d5fe2c1479ff7
maxkram <max.kramer@live.com>
2019-10-31 16:49:48 -0400

    add redux, implement userReducer&userActions 2 header&app components

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -44,10 +44,10 @@ class SignUp extends Component {
     }
   };
 
-  handleChange = event => {
-      const {name, value} = event.target;
-      this.setState({[name]: value})
-  }
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
 
   render() {
     const { displayName, email, password, confirmPassword } = this.state;
