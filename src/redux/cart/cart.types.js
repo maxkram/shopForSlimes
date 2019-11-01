9f53f2b58dd8e8c66c7b2226708f4d0da2674dcf
maxkram <max.kramer@live.com>
2019-11-01 13:10:55 -0400

    update shop data&cart utils in cart reducer

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,5 +1,5 @@
 const CartActionTypes = {
-    TOGGLE_CARD_HIDDEN: "TOGGLE_CARD_HIDDEN"
-  };
-  export default CartActionTypes;
-  
+  TOGGLE_CARD_HIDDEN: "TOGGLE_CARD_HIDDEN",
+  ADD_ITEM: "ATEM_ITEM"
+};
+export default CartActionTypes;
